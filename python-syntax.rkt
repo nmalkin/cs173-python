3aab3fbdc82238dd859db91493f4879bfd372fe5
Nathan Malkin <nmalkin@cs.brown.edu>
2012-10-24 22:05:12 -0400

    Expand surface syntax

--- python-syntax.rkt
+++ python-syntax.rkt
@@ -3,6 +3,14 @@
 (define-type PyExpr
   [PySeq (es : (listof PyExpr))]
   [PyNum (n : number)]
+  [PyBool (b : boolean)]
+  [PyStr (s : string)]
   [PyId (x : symbol)]
+  [PyRaise (expr : PyExr) (cause : PyExpr)]
+  [PyPass ()]
+  [PyIf (test : PyExr) (body : PyExr) (orelse : PyExpr)]
+  [PyBinOp (left : PyExpr) (op : symbol) (right : PyExpr)]
+  [PyUnaryOp (op : symbol) (operand : PyExpr)]
+  [PyLam (args : (listof PyExpr)) (body : PyExpr)]
   [PyApp (fun : PyExpr) (args : (listof PyExpr))])
 
