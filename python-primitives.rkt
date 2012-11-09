6856be18b7e51807226fcdefa657699f7e8f10b1
Sumner Warren <sumner_warren@brown.edu>
2012-11-08 21:11:14 -0500

    Working on fixing macro for type checking builtin prims.

--- python-primitives.rkt
+++ python-primitives.rkt
@@ -1,6 +1,7 @@
 #lang plai-typed
 
 (require "python-core-syntax.rkt"
+         "util.rkt"
          (typed-in racket/string (string-join : ((listof string) string -> string)))
          (typed-in racket/base (number->string : (number -> string))))
 
@@ -25,7 +26,7 @@ primitives here.
     [VNone () "None"]
     [VClosure (env args body) (error 'pretty "Can't print closures yet")]
     [else (error 'pretty (string-append "Cannot print case: " (to-string arg)))]))
-  
+
 
 (define (print arg)
   (display (pretty arg)))
@@ -41,33 +42,39 @@ primitives here.
              (if (and (VObject? arg1) (VObject? arg2)
                       (symbol=? (VObject-antecedent arg1) 'num)
                       (symbol=? (VObject-antecedent arg2) 'num))
-               (let ([mayb-mval1 (VObject-mval arg1)] 
-                     [mayb-mval2 (VObject-mval arg2)]) 
-                 (if (and (some? mayb-mval1) (some? mayb-mval2))
-                   (let ([mval1 (some-v mayb-mval1)]
-                         [mval2 (some-v mayb-mval2)])
-                     (some (VObject 'num (some (MetaNum 
-                                                 (+ (MetaNum-n mval1) 
-                                                    (MetaNum-n mval2))))
-                                    (make-hash empty))))
-                   (none)))
-               (none)))]
+                 (let ([mayb-mval1 (VObject-mval arg1)] 
+                       [mayb-mval2 (VObject-mval arg2)]) 
+                   (if (and (some? mayb-mval1) (some? mayb-mval2))
+                       (let ([mval1 (some-v mayb-mval1)]
+                             [mval2 (some-v mayb-mval2)])
+                         (some (VObject 'num (some (MetaNum 
+                                                    (+ (MetaNum-n mval1) 
+                                                       (MetaNum-n mval2))))
+                                        (make-hash empty))))
+                       (none)))
+                 (none)))]
     ['str (let ([arg (first args)])
             (some (VStr (number->string (MetaNum-n (some-v 
-                                                     (VObject-mval arg)))))))]))
+                                                    (VObject-mval arg)))))))]
+    ['str+ (check-types args 'str 'str
+                        (some (VObject 'str 
+                                       (some (MetaStr
+                                              (string-append (MetaStr-s mval1)
+                                                             (MetaStr-s mval2))))
+                                       (hash empty))))]))
 
 
 (define (dict-str (contents : (hashof CVal CVal)))
   (string-append "{" 
-                (string-append
-                 (string-join
-                       (map (lambda(k)
-                              (string-append (pretty k)
-                                             (string-append 
-                                                 ": "
-                                                 (pretty (some-v (hash-ref contents
-                                                                   k))))))
-                              (hash-keys contents))
-                       ", ")
-                 "}")))
+                 (string-append
+                  (string-join
+                   (map (lambda(k)
+                          (string-append (pretty k)
+                                         (string-append 
+                                          ": "
+                                          (pretty (some-v (hash-ref contents
+                                                                    k))))))
+                        (hash-keys contents))
+                   ", ")
+                  "}")))
 
--- util.rkt
+++ util.rkt
@@ -3,8 +3,8 @@
 (require "python-core-syntax.rkt")
 
 (require
-         (typed-in racket/base 
-                   (hash-for-each : ((hashof 'a 'b) ('c 'd -> 'e) -> void))))
+ (typed-in racket/base 
+           (hash-for-each : ((hashof 'a 'b) ('c 'd -> 'e) -> void))))
 
 ; a file for utility functions that aren't specific to python stuff
 
@@ -13,18 +13,18 @@
 ; values from one to values in the other
 (define (lists->hash [l1 : (listof 'a)] [l2 : (listof 'b)]) : (hashof 'a 'b)
   (local [(define h (make-hash empty))]
-         (begin
-           (map2 (lambda (k v) (hash-set! h k v))
-               l1 l2)
-           h)))
+    (begin
+      (map2 (lambda (k v) (hash-set! h k v))
+            l1 l2)
+      h)))
 
 (test (lists->hash (list "a" "b" "c") (list 1 2 3))
       (let ([h (make-hash empty)])
         (begin
-               (hash-set! h "a" 1)
-               (hash-set! h "b" 2)
-               (hash-set! h "c" 3) 
-               h)))
+          (hash-set! h "a" 1)
+          (hash-set! h "b" 2)
+          (hash-set! h "c" 3) 
+          h)))
 
 (define (immutable-hash-copy h)
   (let ([r (hash empty)])
@@ -34,10 +34,10 @@
 
 (define (seq-ops (ops : (listof CExpr))) : CExpr
   (foldl
-    (lambda (next sofar)
-      (CSeq next sofar))
-    (first ops)
-    (rest ops)))
+   (lambda (next sofar)
+     (CSeq next sofar))
+   (first ops)
+   (rest ops)))
 
 (define (def (name : symbol) (expr : CExpr)) : CExpr
   (CAssign (CId name) expr))
@@ -45,25 +45,28 @@
 (define-syntax (check-types x)
   (syntax-case x ()
     [(check-types args t1 body)
-     #'(let ([arg1 (first args)])
-         (if (and (VObject? arg1) (symbol=? (VObject-antecedent) 'num))
-             (let ([mayb-mval1 (VObject-mval arg1)])
-               (if (some? mayb-mval1)
-                   (let ([mval1 (some-v mayb-mval1)])
-                     body)
-                   (none)))
-             (none)))]
+     ;(with-syntax ([mval1 (datum->syntax x 'mval1)])
+       #'(let ([arg1 (first args)])
+           (if (and (VObject? arg1) (symbol=? (VObject-antecedent) 'num))
+               (let ([mayb-mval1 (VObject-mval arg1)])
+                 (if (some? mayb-mval1)
+                     (let ([mval1 (some-v mayb-mval1)])
+                       body)
+                     (none)))
+               (none)))]
     [(check-types args t1 t2 body)
-     #'(let ([arg1 (first args)]
-             [arg2 (second args)])
-         (if (and (VObject? arg1) (VObject? arg2)
-                  (symbol=? (VObject-antecedent arg1) t1)
-                  (symbol=? (VObject-antecedent arg2) t2))
-             (let ([mayb-mval1 (VObject-mval arg1)]
-                   [mayb-mval2 (VObject-mval arg2)])
-               (if (and (some? mayb-mval1) (some? mayb-mval2))
-                   (let ([mval1 (some-v mayb-mval1)]
-                         [mval2 (some-v mayb-mval2)])
-                     body)
-                   (none)))
-             (none)))]))
+     (with-syntax ([mval1 #'mval1]
+                   [mval2 #'mval2])
+       #'(let ([arg1 (first args)]
+               [arg2 (second args)])
+           (if (and (VObject? arg1) (VObject? arg2)
+                    (symbol=? (VObject-antecedent arg1) t1)
+                    (symbol=? (VObject-antecedent arg2) t2))
+               (let ([mayb-mval1 (VObject-mval arg1)]
+                     [mayb-mval2 (VObject-mval arg2)])
+                 (if (and (some? mayb-mval1) (some? mayb-mval2))
+                     (let ([mval1 (some-v mayb-mval1)]
+                           [mval2 (some-v mayb-mval2)])
+                       body)
+                     (none)))
+               (none))))]))
